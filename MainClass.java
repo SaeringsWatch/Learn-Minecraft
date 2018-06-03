eea50d46a77b877134062f078b61a2f7d9733ffc
IslandPro_Ninja <IslandProNinja@gmail.com>
2018-06-02 20:13:31 -0700

    Create MainClass.java
    Basic Creation of the main class

---
+++ MainClass.java
@@ -0,0 +1,34 @@
+package package.name.here
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MainClass extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		new ListenerClass(this);
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		
+		if (cmd.getName().equalsIgnoreCase("COMMAND1") && sender instanceof Player) {
+			
+			Player player = (Player) sender;
+			
+			return true;
+			
+		}
+		
+		return false;	
+		
+	}
+	
+}
